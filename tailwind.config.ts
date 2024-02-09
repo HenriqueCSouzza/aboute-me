ecb8a5aa4853bfd5d26cdcf93c3b7e27762f2411
Henrique Souza <henrique.csouzza@gmail.com>
2024-02-09 14:14:57 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        primary: "Poppins, sans-serif",
+      },
+      colors: {
+        "light-content": "#A7A7A7",
+        "dark-heading": "#1f2428",
+        "dark-content": "#666666",
+        "light-heading": "#CCCCCC",
+        "dark-mode": "#191919",
+        "dark-card": "#363636",
+        "green-text": "#018C0F",
+        greenbg: "#D7FFE0",
       },
     },
   },
