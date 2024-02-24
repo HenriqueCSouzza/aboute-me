5720a48dcc020a9c1221035ede8a2a55ad7e3d37
Henrique Souza <henrique.csouzza@gmail.com>
2024-02-24 05:48:38 -0300

    include speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -34,6 +35,7 @@ export default function RootLayout({
         }
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
