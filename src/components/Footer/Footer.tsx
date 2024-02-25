688853972d31727541efba07949a6873d5e76259
Henrique Souza <henrique.csouzza@gmail.com>
2024-02-25 14:56:01 -0300

    change footer and header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,33 +9,33 @@ export default function Footer() {
   const { linkedin, github, twitter } = socialMediaUrl;
   return (
     <footer className="w-full mx-auto py-10 bg-footer dark:bg-gray-300">
-      <div className="container max-width flex xs:flex-row justify-between gap-4">
-        <div>
+      <div className="container max-width flex flex-col sm:flex-row justify-between gap-4">
+        <div className="flex flex-col  items-center sm:items-start">
           <Text color="contrast" className="font-bold">
             Como me encontrar!
           </Text>
-          <div className="flex gap-6 items-end mt-4">
+          <div className="flex gap-6 mt-4">
             <a
               href={`mailto:${email}`}
               target="_blank"
               rel="noreferrer noopener"
             >
-              <MdEmail className="dark:fill-dark-heading fill-light-heading text-xl cursor-pointer" />
+              <MdEmail className="dark:fill-dark-heading fill-light-heading text-3xl cursor-pointer" />
             </a>
             <a href={linkedin} target="_blank" rel="noreferrer noopener">
-              <FaLinkedin className="dark:fill-dark-heading fill-light-heading text-xl cursor-pointer" />
+              <FaLinkedin className="dark:fill-dark-heading fill-light-heading text-3xl cursor-pointer" />
             </a>
             <a href={github} target="_blank" rel="noreferrer noopener">
-              <FaGithub className="dark:fill-dark-heading fill-light-heading text-xl cursor-pointer" />
+              <FaGithub className="dark:fill-dark-heading fill-light-heading text-3xl cursor-pointer" />
             </a>
             <a href={twitter} target="_blank" rel="noreferrer noopener">
-              <FaXTwitter className="dark:fill-dark-heading fill-light-heading text-xl cursor-pointer" />
+              <FaXTwitter className="dark:fill-dark-heading fill-light-heading text-3xl cursor-pointer" />
             </a>
           </div>
         </div>
-        <div className="flex justify-end items-end">
+        <div className="flex justify-center sm:justify-end items-end text-center">
           <Text size="sm" color="contrast">
-            Made © 2024 Henrique Souza
+            © 2024 Henrique Souza
           </Text>
         </div>
       </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,49 +35,28 @@ function Header() {
   };
 
   return (
-    <header className="container mx-auto md:flex justify-between max-width">
-      <div className="flex justify-between items-center py-2 md:py-10">
+    <header className="container mx-auto max-width py-2 md:py-10">
+      <div className="flex items-center justify-between">
         <NavLink
           href="/"
-          className="text-lg text-black dark:text-light-content md:text-lg xl:text-lg xl:leading-tight font-bold"
+          className="text-base sm:text-lg text-gradient md:text-lg xl:text-lg xl:leading-tight font-bold"
         >
-          Henrique Souza
+          {"{ Henrique Souza }"}
         </NavLink>
-        <div onClick={toggleClass} className="cursor-pointer">
-          <svg
-            className="stroke-dark-heading dark:stroke-white md:hidden"
-            width="25"
-            height="20"
-            viewBox="0 0 16 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.4375 1.3125H14.5625M1.4375 11.3125H14.5625H1.4375ZM1.4375 6.3125H14.5625H1.4375Z"
-              strokeWidth="1.875"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+        <nav className={`text-center flex `}>
+          <ul className="dark:text-light-content font-medium flex gap-2 items-center md:space-x-5 md:mr-10">
+            {React.Children.toArray(
+              linkList.map(({ label, link }) => (
+                <li className="font-light text-base sm:text-lg">
+                  <NavLink href={link} onClick={toggleClass}>
+                    {label["pt-BR"]}
+                  </NavLink>
+                </li>
+              ))
+            )}
+          </ul>
+        </nav>
       </div>
-      <nav
-        className={` ${
-          !isOpen ? "hidden" : null
-        } text-center md:flex justify-between`}
-      >
-        <ul className="dark:text-light-content font-medium md:flex items-center md:space-x-5 md:mr-10">
-          {React.Children.toArray(
-            linkList.map(({ label, link }) => (
-              <li className="pb-1 md:pb-0 font-light">
-                <NavLink href={link} onClick={toggleClass}>
-                  {label["pt-BR"]}
-                </NavLink>
-              </li>
-            ))
-          )}
-        </ul>
-      </nav>
     </header>
   );
 }
