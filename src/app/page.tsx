efc36d7c4b221f41a31911dff4cb2e8f97a28b33
Henrique Souza <henrique.csouzza@gmail.com>
2024-07-15 11:06:48 -0300

    fix headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { personalDetails } from "@/data/info";
 import Title from "@/components/design/Title";
+import Text from "@/components/design/Text";
 import React from "react";
 
 type LangType = "en-US" | "pt-BR";
@@ -19,15 +20,13 @@ export default async function AboutPage({ searchParams }: AboutPageType) {
           <Title size="h4" tag="h1">
             {presentation[lang] + " " + name[lang]}
           </Title>
-          <Title size="h4" tag="h1">
+          <Text size="xl">
             {phraseJob[lang]}{" "}
             <span className="bg-clip-text bg-gradient text-transparent">
               {job[lang]}
             </span>
-          </Title>
-          <Title size="h4" tag="h2">
-            {tagline[lang]}
-          </Title>
+          </Text>
+          <Text size="xl">{tagline[lang]}</Text>
         </div>
         <div className="flex justify-center items-center ">
           <picture className="w-1/2 sm:w-1/3 md:ml-auto ">
